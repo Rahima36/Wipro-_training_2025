2d01d745697996e0ec8d2a9c26366ac99978b5e8
Rahima36 <164537377+Rahima36@users.noreply.github.com>
2025-08-10 21:26:42 +0530

    Update SortStringList.java

--- Day 28-07-25/Exercise/MethodReference/src/com/wipro/methodref/SortStringList.java
+++ Day 28-07-25/Exercise/MethodReference/src/com/wipro/methodref/SortStringList.java
@@ -6,10 +6,10 @@ import java.util.stream.Collectors;
 
 public class SortStringList {
 	public static void main(String[] args) {
-		// Create a list of strings
+		
         List<String> li = Arrays.asList("Shubham", "Anand", "Sham", "Tejas", "Prashant");
 
-        // Sort using stream and method reference (compareTo)
+        
         List<String> sortedList = li.stream()
                                        .sorted(String::compareTo)
                                        .collect(Collectors.toList());
