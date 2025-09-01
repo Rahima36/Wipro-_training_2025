57afa4e537fa0e93b64ba37098acc048316b9e9b
Rahima36 <164537377+Rahima36@users.noreply.github.com>
2025-09-01 08:13:22 +0530

    Add files via upload

---
+++ capstone_project/order-service (1)/order-service/src/main/java/com/wipro/order_service/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.wipro.order_service.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.wipro.order_service.model.OrderEntity;
+
+public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+	  List<OrderEntity> findByUserId(Long userId);
+	}
