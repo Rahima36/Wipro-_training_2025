cc40b1a0e6e208806b07629416bc71eaa609cda2
Rahima36 <164537377+Rahima36@users.noreply.github.com>
2025-09-01 08:21:27 +0530

    Add files via upload

---
+++ capstone_project/Backend/product-service/product-service/src/main/java/com/wipro/product_service/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.wipro.product_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.wipro.product_service.model.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+}
